e5453435b1d925c701493e1e5e9fb338eb1eb75a
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2025-03-09 22:33:30 +0200

    first commit

--- js/slick slider/slick.js
+++ js/slick slider/slick.js
@@ -79,22 +79,83 @@ $(document).ready(function () {
     customPaging: function (slider, i) {
       return "<div></div>";
     },
-    arrows: false, // You can set this to true if you want navigation arrows
+    arrows: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true, // Optional: Enable autoplay
-    autoplaySpeed: 2000, // Optional: Set autoplay speed in milliseconds
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 
-  // Sync the active dot with the current slide
   $(".SliderBanner").on("afterChange", function (event, slick, currentSlide) {
     $(".dots-SliderBanner div").removeClass("active");
     $(".dots-SliderBanner div").eq(currentSlide).addClass("active");
   });
 
-  // Initialize the first dot as active
   $(".dots-SliderBanner div").first().addClass("active");
 });
 //  End Slider 2
+// Start slider 3
+$(document).ready(function () {
+  $(".sliderProducts").slick({
+    dots: true,
+    appendDots: $(".dots-sliderProducts"),
+    prevArrow: $(".leftProduct"),
+    nextArrow: $(".rightProduct"),
+    customPaging: function (slider, i) {
+      return '<div class="dot"></div>';
+    },
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          centerMode: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+        },
+      },
+    ],
+  });
+
+  function updateActiveDot(currentSlide) {
+    $(".dots-sliderProducts .dot").removeClass("active");
+    $(".dots-sliderProducts .dot").eq(currentSlide).addClass("active");
+  }
+  $(".sliderProducts").on("afterChange", function (event, slick, currentSlide) {
+    updateActiveDot(currentSlide);
+  });
+
+  updateActiveDot(0);
+
+  $(".dots-sliderProducts .dot").on("click", function () {
+    var slideIndex = $(this).index();
+    $(".sliderProducts").slick("slickGoTo", slideIndex);
+  });
+});
