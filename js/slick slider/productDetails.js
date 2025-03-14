d6863de13cc27259b4220b225129dbac78926a98
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2025-03-14 11:22:02 +0200

    Update

--- js/slick slider/productDetails.js
+++ js/slick slider/productDetails.js
@@ -35,3 +35,25 @@ $(document).ready(function () {
   $(".slick -current.slick-active").addClass("active");
   $(".imagesProducts").slick("slickInit");
 });
+////////////////////////////////////////////////////////////////////////
+// Plus Minus Weight
+$(document).ready(function () {
+  $(".fxAmount-product").each(function () {
+    let container = $(this);
+    let minusBtn = container.find("button:first-child");
+    let plusBtn = container.find("button:last-child");
+    let weightText = container.find("p span");
+
+    minusBtn.click(function () {
+      let currentWeight = parseInt(weightText.text());
+      if (currentWeight > 1) {
+        weightText.text(currentWeight - 1);
+      }
+    });
+
+    plusBtn.click(function () {
+      let currentWeight = parseInt(weightText.text());
+      weightText.text(currentWeight + 1);
+    });
+  });
+});
